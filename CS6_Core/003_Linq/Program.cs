cd88d5a452fec57677a08de48b7e90a9317e45aa
djurela <davor.jurela@gmail.com>
2017-05-06 13:43:52 +0200

    Comprehension syntax used

--- CS6_Core/003_Linq/Program.cs
+++ CS6_Core/003_Linq/Program.cs
@@ -13,7 +13,16 @@ namespace ConsoleApplication
             string[] names = new string[]{"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
             //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
             //var query = names.Where(NameLongerThanFour);
-            var query = names.Where(name => name.Length > 4).OrderBy(name => name.Length).ThenBy(name => name);
+            //var query = names.Where(name => name.Length > 4).OrderBy(name => name.Length).ThenBy(name => name);
+            //var query = names.Where(name => name.Length > 4).OrderBy(name => name.Length).ThenBy(name => name).Skip(1).Take(2);
+            //var query = from name in names
+            //            where name.Length > 4
+            //            orderby name.Length, name
+            //            select name;
+            var query = (from name in names
+                        where name.Length > 4
+                        orderby name.Length, name
+                        select name).Skip(1).Take(2);
             foreach(var item in query)
                 WriteLine($"  {item}");
         }
