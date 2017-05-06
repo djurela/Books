bec4dc04c721cf6bb9d46db4721bcf0c1bc5ae31
djurela <davor.jurela@gmail.com>
2017-05-06 11:47:34 +0200

    Linq queries

---
+++ CS6_Core/003_Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("Linq examles");
+
+            string[] names = new string[]{"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
+            //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+            //var query = names.Where(NameLongerThanFour);
+            var query = names.Where(name => name.Length > 4).OrderBy(name => name.Length).ThenBy(name => name);
+            foreach(var item in query)
+                WriteLine($"  {item}");
+        }
+
+        private static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+    }
+}
