6bc16dea508241adcf70097135af5814a50faaf5
djurela <davor.jurela@gmail.com>
2017-05-10 20:22:57 +0200

    My LINQ Extensions

---
+++ CS6_Core/007_MyLINQExtensions/src/app/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("Linq examles");
+
+            string[] names = new string[]{"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
+            var query = names.ProcessSequence().Where(name => name.Length > 4).OrderBy(name => name.Length).ThenBy(name => name);
+            foreach(var item in query)
+                WriteLine($"  {item}");
+            WriteLine($"Sumarise sequence: {names.SumariseSequence()}");
+            Console.Write("Press any key to continue ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CS6_Core/007_MyLINQExtensions/src/library/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class MyLINQExtensions
+    {
+        public static IEnumerable<T> ProcessSequence<T>(this IEnumerable<T> sequence)
+        {    
+            return sequence.Skip(2);
+        }
+
+        public static long SumariseSequence<T>(this IEnumerable<T> sequence)
+        {
+            return sequence.LongCount();
+        }
+    }
+}
