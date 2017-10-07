f68163a031c0a7237bf848e7be1c214d0d690d86
djurela <davor.jurela@gmail.com>
2017-10-07 15:13:41 +0200

    Prva verzija

---
+++ CS6_Core/008_Enums/Person.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _008_Enums
+{
+    public class Person
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+
+        public WondersEnum BucketList;
+    }
+}
---
+++ CS6_Core/008_Enums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+
+namespace _008_Enums
+{
+    [System.Flags]
+    public enum WondersEnum
+    {
+        None = 0,
+        GreatPyramid = 1,
+        HangingGardens = 1 << 1,
+        StatueOfZeus = 1 << 2,
+        TempleOfArtemis = 1 << 3,
+        Mausoleum = 1 << 4,
+        Colossus = 1 << 5,
+        Lighthouse = 1 << 6
+    }
+
+    class Program
+    {
+        #region Main
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person = new Person{Name = "Miš Miško", DateOfBirth = new DateTime(2003, 09, 19)};
+                person.BucketList = WondersEnum.HangingGardens | WondersEnum.Mausoleum;
+                WriteLine($"Osoba {person.Name} je rođena {person.DateOfBirth:d MMM yyyy}.");
+                WriteLine($"Želi posjetiti: {person.BucketList}");
+            }
+            catch(Exception excp)
+            {
+                WriteLine($"ERROR - {excp.Message}.");
+            }
+            finally
+            {
+                WriteLine("Done.");
+            }
+        }
+        #endregion
+    }
+}
