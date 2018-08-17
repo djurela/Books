d730215c4116aa473a1c97e0b7d3dc871ec3af93
Davor Jurela <davor.jurela@mireo.hr>
2018-08-17 14:29:23 +0200

    Task example

---
+++ CS6_Core/013_Tasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace _013_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = Stopwatch.StartNew();
+            // WriteLine("Running methods synchronously on one thread.");
+            // MethodA();
+            // MethodB();
+            // MethodC();
+            WriteLine("Running methods asynchronously on multiple threads.");
+            var taskA = new Task(MethodA);
+            taskA.Start();
+            var taskB = Task.Factory.StartNew(MethodB);
+            var taskC = Task.Run(new Action(MethodC));
+            Task[] tasks = { taskA, taskB, taskC };
+            Task.WaitAll(tasks);
+ 
+            WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+            WriteLine("Press ENTER to end.");
+            ReadLine();
+        }
+
+        static void MethodA()
+        {
+            WriteLine("Starting Method A...");
+            Thread.Sleep(3000); // simulate three seconds of work
+            WriteLine("Finished Method A.");
+        }
+        static void MethodB()
+        {
+            WriteLine("Starting Method B...");
+            Thread.Sleep(2000); // simulate two seconds of work
+            WriteLine("Finished Method B.");
+        }
+        static void MethodC()
+        {
+            WriteLine("Starting Method C...");
+            Thread.Sleep(1000); // simulate one second of work
+            WriteLine("Finished Method C.");
+        }
+    }
+}
